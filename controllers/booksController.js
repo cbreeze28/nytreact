6526336a93bd75961c21af2d372a4bf03f55d7ea
Chris B <38058127+cbreeze28@users.noreply.github.com>
2018-10-25 09:05:04 -0400

    nyt api

--- controllers/booksController.js
+++
@@ -1,9 +0,0 @@
-const db = require("../models/book");
-
-module.exports = {
-findAllBooks : function(req, res){
-    db.books.find(req.query).then(dbModel => res.json(dbModel)).catch(err => res.status(500).json(err));
-
-
-}
-};
